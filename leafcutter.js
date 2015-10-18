2f4b99ed334c1a0d2e759230d604f57ff594c097
pieterprovoost <pieterprovoost@gmail.com>
2015-10-18 22:14:57 +0200

    refactoring

--- leafcutter.js
+++ leafcutter.js
@@ -1,72 +1,53 @@
-angular.module('leafcutter', [])
-	.directive('leaflet', function(leafcutterdata) {
-		return {
-			restrict: "E",
-			replace: true,
-			scope: {
-				view: '=?',
-				options: '=?'
-			},
-			transclude: false,
-			template: '<div class="leafcutter"></div>',
-			controller: ["$scope", function ($scope) {
-			}],
-			link: function(scope, element, attrs, controller) {
-				
-				var default_view = {
-					lon: 10, 
-					lat: 30, 
-					zoom: 2
-				};
+var leafcutter = angular.module("leafcutter", []);
 
-				var default_options = {
-					attributionControl: false
-				};
-
-				scope.view = angular.extend(default_view, scope.view);
-				scope.options = angular.extend(default_options, scope.options);
-
-				if (!angular.isDefined(scope.options.layers)) {
-					scope.options.layers = [L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {})];
+leafcutter.directive("leaflet", function(leafcuttermaps) {
+	return {
+		restrict: "EA",
+		scope: {
+			name: "="
+        },
+		link: function(scope, element, attrs) {
+			scope.layers = [];
+			scope.map = L.map(element[0], {
+				attributionControl: false
+			}).setView([30, 20], 2);
+			L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {}).addTo(scope.map);
+			leafcuttermaps.setMap(attrs.map, {
+				map: scope.map,
+				addlayer: function(layer) {
+					layer.addTo(scope.map);
+					scope.layers.push(layer);
+				},
+				reset: function() {
+					for (l in scope.layers) {
+						scope.map.removeLayer(scope.layers[l]);
+					}
+					scope.layers = [];
 				}
-
-				scope.map = L.map(element[0], scope.options);
-				scope.map.setView([scope.view.lat, scope.view.lon], scope.view.zoom);
-
-				leafcutterdata.setMap(attrs.id, scope.map);
-
-				scope.$on('$destroy', function () {
-					leafcutterdata.reset();
-				});
-
-			}
+			});
+			scope.$on('$destroy', function () {
+				leafcuttermaps.reset();
+			});
 		}
-	});
+	};
+});
 
-angular.module('leafcutter').service('leafcutterdata', function($q) {
-
+leafcutter.service("leafcuttermaps", function($q) {
 	var maps = {};
-
 	this.setMap  = function(id, map) {
 		if (maps.hasOwnProperty(id)) {
 			maps[id].resolve(map);
 		} else {
-			var deferred = $q.defer();
-			deferred.resolve(map);
-			maps[id] = deferred;
+			maps[id] = $q.defer().resolve(map);
 		}
 	};
-
 	this.getMap = function(id) {
 		if (!maps.hasOwnProperty(id)) {
-			var deferred = $q.defer();
-			maps[id] = deferred;
+			maps[id] = $q.defer();
 		}
 		return maps[id].promise;
 	};
-
 	this.reset = function() {
 		maps = {};
 	};
-
 });
